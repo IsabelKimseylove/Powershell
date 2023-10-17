0fee952d0222e1736353b60f12499877333eda6f
IsabelKimseylove <145058670+IsabelKimseylove@users.noreply.github.com>
2023-10-17 11:47:39 -0700

    Delete AD directory

--- AD/Bitlocker AD Script.ps1
+++
@@ -1,6 +0,0 @@
-$computername = Read-Host "What is the computer name?"
-$objComputer = Get-ADComputer $computername
-$Bitlocker_Object = Get-ADObject -Filter {objectclass -eq 'msFVE-RecoveryInformation'} -SearchBase $objComputer.DistinguishedName -Properties 'msFVE-RecoveryPassword'
-Write-Host $Bitlocker_Object
-
-
